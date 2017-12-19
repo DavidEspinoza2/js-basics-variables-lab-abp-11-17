1ee492a837e5db669672f055927c4a6ec3bcd3fc
David Espinoza <davidespinoza@att.net>
2017-12-19 04:49:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const companyName = 'Scuber';
-companyName = 'companyName';
+companyName = 'specialCompany';
 
 const mostProfitableNeighborhood = 'Chelsea';
 mostProfitableNeighborhood = 'Chelsea';
